650311b673ce300a55c33397f5048a3137e64ae7
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-06-01 11:39:12 +0200

    Finised style categories buttons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,13 +12,28 @@ const Sidebar = () => (
       flexDirection: { md: "column" },
     }}
   >
-    {categories.map(category => (
+    {categories.map((category) => (
       <button
-        className="category=btn"
-        style={{ background: (category.name === selectedCategory) & "#FC1503" }}
+        className="category-btn"
+        style={{
+          background: (category.name === selectedCategory) & "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
       >
-        <span>{category.icon}</span>
-        <span>{category.name}</span>
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
+        >
+          {category.name}
+        </span>
       </button>
     ))}
   </Stack>
