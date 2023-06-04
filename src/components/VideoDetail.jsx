23e488852119111a667e1e38a18fd66e14e5cc7a
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-06-04 20:01:57 +0200

    Added ReactPLayer component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 import { Stack, Box } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then(data =>
+      setVideoDetail(data.items[0])
+    );
+  }, [id]);
+
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-            <ReactPlayer />
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
           </Box>
         </Box>
       </Stack>
