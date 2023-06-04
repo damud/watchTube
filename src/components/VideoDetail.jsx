08fd9e4b9a110b2ace8cd5e1134210aa1a91b61b
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-06-04 11:38:27 +0200

    Started creating VideoDetail component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,5 +1,18 @@
+import ReactPlayer from "react-player";
+import { Stack, Box } from "@mui/material";
+
 const VideoDetail = () => {
-  return <div>VideoDetail</div>;
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default VideoDetail;
